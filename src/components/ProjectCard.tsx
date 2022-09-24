71d39203bdaf5b936de9241baa1794d271f81fd5
IlyasOussama <zghariilyasoussama@gmail.com>
2022-09-24 16:06:58 +0100

    projectCard li click

--- src/components/ProjectCard.tsx
+++ src/components/ProjectCard.tsx
@@ -23,36 +23,44 @@ export default function ProjectCard({
   className,
 }: ProjectCardProps) {
   return (
-    <div className="scale-100 hover:scale-[1.02] active:scale-[0.97] motion-safe:transform-gpu overflow-hidden bg-white rounded shadow dark:bg-gray-900 dark:shadow-gray-800">
-      <div className="p-5">
-        <div className="relative">
-          <a href="#" title="" className="block aspect-w-4 aspect-h-3">
-            <Image
-              className="object-cover w-full h-full"
-              height={400}
-              width={700}
-              src={projectImage}
-              alt=""
-            />
-          </a>
+    <li className="scale-100 hover:scale-[1.02] active:scale-[0.97] motion-safe:transform-gpu overflow-hidden bg-white rounded shadow dark:bg-gray-900 dark:shadow-gray-800">
+      <Link as={`/projects/${projectLink}`} href="/projects/[slug]">
+        <div className="p-5">
+          <div className="relative">
+            <a href="#" title="" className="block aspect-w-4 aspect-h-3">
+              <Image
+                className="object-cover w-full h-full"
+                height={400}
+                width={700}
+                src={projectImage}
+                alt=""
+              />
+            </a>
+          </div>
+
+          <p className="mt-5 text-2xl font-semibold">
+            <Link as={`/projects/${projectLink}`} href="/projects/[slug]">
+              <a href="#" title="" className="text-black dark:text-white">
+                {" "}
+                {projectName}{" "}
+              </a>
+            </Link>
+            <a href="#" title="" className="text-black dark:text-white">
+              {" "}
+              {projectName}{" "}
+            </a>
+          </p>
+          <p className="mt-4 text-base text-gray-600 dark:text-gray-400">
+            {projectDescription}
+          </p>
+
+          <Link as={`/projects/${projectLink}`} href="/projects/[slug]">
+            <a className="animated-underline dark:aninated-underline-dark mt-2 inline-block font-medium">
+              Learn More
+            </a>
+          </Link>
         </div>
-
-        <p className="mt-5 text-2xl font-semibold">
-          <a href="#" title="" className="text-black dark:text-white">
-            {" "}
-            {projectName}{" "}
-          </a>
-        </p>
-        <p className="mt-4 text-base text-gray-600 dark:text-gray-400">
-          {projectDescription}
-        </p>
-
-        <Link as={`/projects/${projectLink}`} href="/projects/[slug]">
-          <a className="animated-underline dark:aninated-underline-dark mt-2 inline-block font-medium">
-            Learn More
-          </a>
-        </Link>
-      </div>
-    </div>
+      </Link>
+    </li>
   );
 }
