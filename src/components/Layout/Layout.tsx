e3986f4f7a83f99eb3be2700ed7c5bdc81ede623
IlyasOussama <zghariilyasoussama@gmail.com>
2022-09-25 23:37:49 +0100

    nb

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -5,12 +5,18 @@ import MobileMenu from "./MobileMenu";
 
 const Layout: React.FC<PropsWithChildren> = ({ children }) => {
   return (
-    <div className="max-w-6xl mx-auto">
-      <MobileMenu />
-      <Navbar />
-      {children}
-      <Footer />
-    </div>
+    <>
+      <div className="bg-red-500 text-xl font-semibold p-3 text-center">
+        Still working on it, Excuse the unfinished work and the dummy data, have
+        a great day!{" "}
+      </div>
+      <div className="max-w-6xl mx-auto">
+        <MobileMenu />
+        <Navbar />
+        {children}
+        <Footer />
+      </div>
+    </>
   );
 };
 
